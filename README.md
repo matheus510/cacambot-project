378d73ed40307c3a3eee68c982e01cd9eb81ccc5
Matheus Sousa <matheus510.fonseca@gmail.com>
2017-08-21 19:45:57 -0300

    heheheh

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# cacambot-project
-O site e produtos relacionados ao Cacambot
+# cacambot
+
+> n
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="app">
+    <div class="section1">
+      <div class="header">
+        {{ header }}
+      </div>
+      <div class="subheader">
+        <h3>{{ subheader }}</h3>
+      </div>
+    </div>
+    <div class="section2">
+      <div class="content">
+        <h2>{{ title }}</h2>
+      </div>  
+    </div>
+    <div class="section3">
+      
+    </div>
+    <div class="section4">
+      
+    </div>
+    <div class="section5">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      header: 'CaçambotW200',
+      subheader: 'Ao fim do dia, todos se tornam entulho...'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import url('https://fonts.googleapis.com/css?family=Raleway:800');
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
+  height: 100%;
+  width: 100%;
+  font-family: 'Raleway';
+}
+
+html, body { 
+    margin: 0; 
+    padding: 0; 
+    width: 100%; 
+    height: 100%;
+}
+
+h1, h2 {
+  font-weight: normal;
+  margin: 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.section1 {
+  background: url('assets/img/s-1.png') no-repeat 50% 0%;
+  height: 50%;
+}
+.section2 {
+  background: url('assets/img/s-2.png') no-repeat 0%;
+  height: 50%;
+}
+
+.header {
+  font-size: 4em;
+  position: absolute;
+  top: 15%;
+  left: 10%;
+}
+.subheader {
+  font-size: 2em;
+  position: absolute;
+  top: 25%;
+  left: 40%;
+  background-color: rgba(0,0,0,0.2);
+  box-sizing: border-box;
+  padding: 0 5%;
+}
+.content {
+  color: white;
+  padding: 0;
+}
+</style>
